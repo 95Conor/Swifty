72a3b014dcdf056601b0e02ed8b1870fc3ade93e
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-02-12 19:03:57 +0000

    Allowed razor page modification at runtime

--- Swifty/Startup.cs
+++ Swifty/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 
 namespace Swifty
 {
@@ -24,6 +25,9 @@ namespace Swifty
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
