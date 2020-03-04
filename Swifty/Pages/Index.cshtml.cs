30d5d86517f7a5833706af386c860f2e7b1a894c
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-04 08:58:08 +0000

    Add policy to setup pages and add admins (commented)

--- Swifty/Pages/Index.cshtml.cs
+++ Swifty/Pages/Index.cshtml.cs
@@ -5,21 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Swifty.Core.Entities;
+using Swifty.Data.Repositories;
 
 namespace Swifty.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly SwiftyRepository<Admin> swiftyRepository;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, SwiftyRepository<Admin> swiftyRepository)
         {
             _logger = logger;
+            this.swiftyRepository = swiftyRepository;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            //Admin me = new Admin() { Email = "conor.maher@tigerbay.co.uk" };
+            //Admin rich = new Admin() { Email = "richard.hodge@tigerbay.co.uk" };
 
+            //await swiftyRepository.AddAsync(me);
+            //await swiftyRepository.AddAsync(rich);
         }
     }
 }
--- Swifty/Pages/Setup/SkillLevel/Index.cshtml.cs
+++ Swifty/Pages/Setup/SkillLevel/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using Swifty.Data.Repositories;
 
 namespace Swifty.Web
 {
+    [Authorize(Policy = "IsAdmin")]
     public class IndexModel : PageModel
     {
         private readonly SwiftyRepository<SkillLevel> swiftyRepository;
