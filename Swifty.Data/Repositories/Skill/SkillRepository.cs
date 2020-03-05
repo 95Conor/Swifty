a517c60913546f4f021ca56772a74a1d9f8a09bc
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-05 19:30:19 +0000

    Archive skills when deleted rather than deleting entity

--- Swifty.Data/Repositories/Skill/SkillRepository.cs
+++ Swifty.Data/Repositories/Skill/SkillRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Swifty.Core.Entities;
 using Swifty.Data.Context;
 
@@ -9,5 +10,11 @@ namespace Swifty.Data.Repositories
     public class SkillRepository : SwiftyRepository<Skill>
     {
         public SkillRepository(SwiftyContext context) : base(context) { }
+
+        public override Task DeleteAsync(Skill entity)
+        {
+            entity.IsArchived = true;
+            return base.UpdateAsync(entity);
+        }
     }
 }
