1aeeafb0e61fbf36dd86955244cc9968b3bef6dd
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-09 21:30:07 +0000

    Removed some leftover comments

--- Swifty.Data/EntityConfiguration/Skill/SkillSetConfiguration.cs
+++ Swifty.Data/EntityConfiguration/Skill/SkillSetConfiguration.cs
@@ -12,16 +12,6 @@ namespace Swifty.Data.EntityConfiguration
         public void Configure(EntityTypeBuilder<SkillSet> builder)
         {
             builder.HasKey(skillSet => skillSet.Id);
-
-    //        modelBuilder.Entity<Grade>()
-    //.HasMany(g => g.EligibleGrades)
-    //.WithMany() // <- parameterless because there's no 2nd (inverse) collection
-    //.Map(m =>
-    //{
-    //    m.ToTable("TBLGRADESRELATIONSHIPS");
-    //    m.MapLeftKey("GRADEID");
-    //    m.MapRightKey("ELIGIBLEGRADEID");
-    //});
         }
     }
 }
