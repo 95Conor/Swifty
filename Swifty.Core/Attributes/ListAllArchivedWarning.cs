b5703e3b645c833829fdc73e1a397784a63fa4fd
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-10 21:12:03 +0000

    Add reluctant comments so future me doesn't hate present me

--- Swifty.Core/Attributes/ListAllArchivedWarning.cs
+++ Swifty.Core/Attributes/ListAllArchivedWarning.cs
@@ -7,5 +7,6 @@ namespace Swifty.Core.Attributes
     [Obsolete("Repository warning: ListAllAsync() does not filter archived entities. Use ListAllNonArchivedAsync() if you wish to achieve this.")]
     public class ListAllArchivedWarning : System.Attribute
     {
+        // This isn't pretty, obsolete is not really what we want, but a nice little way to get a compiler warning nonetheless.
     }
 }
