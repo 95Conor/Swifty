bdf452d9a7d68b29f572901a760f234539e68d62
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-19 23:34:33 +0000

    Removed unnecessary code

--- Swifty/Pages/Setup/Skill/Index.cshtml.cs
+++ Swifty/Pages/Setup/Skill/Index.cshtml.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Swifty.Data.Contracts.Repositories;
-using Swifty.Data.Extensions;
 using Entities = Swifty.Core.Entities;
 
 namespace Swifty.Web.Pages.Setup.Skill
@@ -15,16 +12,10 @@ namespace Swifty.Web.Pages.Setup.Skill
     public class IndexModel : PageModel
     {
         private readonly IBaseArchiveableRepository<Entities.Skill> _skillRepository;
-        private readonly IBaseArchiveableRepository<Entities.SkillArea> _skillAreaRepository;
-        private readonly IBaseArchiveableRepository<Entities.SkillLevel> _skillLevelRepository;
 
-        public IndexModel(IBaseArchiveableRepository<Entities.Skill> skillRepository, 
-            IBaseArchiveableRepository<Entities.SkillArea> skillAreaRepository,
-            IBaseArchiveableRepository<Entities.SkillLevel> skillLevelRepository)
+        public IndexModel(IBaseArchiveableRepository<Entities.Skill> skillRepository)
         {
             _skillRepository = skillRepository;
-            _skillAreaRepository = skillAreaRepository;
-            _skillLevelRepository = skillLevelRepository;
         }
 
         public List<Entities.Skill> AllSkills { get; set; }
