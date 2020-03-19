e6ba59d354570ba5cf5692acd047671e6411d66f
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-19 15:48:24 +0000

    Added IBaseArchiveableRepositoryExtensions for building select lists excluding archived entities

---
+++ Swifty.Data/Extensions/IBaseArchiveableRepositoryExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Swifty.Core.Contracts.Entities;
+using Swifty.Data.Contracts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swifty.Data.Extensions
+{
+    public static class IBaseArchiveableRepositoryExtensions
+    {
+        public static async Task<SelectList> BuildSelectListAsync<T>(this IBaseArchiveableRepository<T> repository, string dataTextField, string dataGroupField = null) where T : class, IArchiveableEntity
+        {
+            var list = await repository.ListAllAsync();
+
+            SelectList selectListItems = new SelectList(list, nameof(IEntityBase.Id), dataTextField, null, dataGroupField);
+
+            return selectListItems;
+        }
+
+        public static async Task<SelectList> BuildFilteredSelectListAsync<T>(this IBaseArchiveableRepository<T> repository, Func<T, bool> filterExpression, string dataTextField, string dataGroupField = null) where T : class, IArchiveableEntity
+        {
+            var list = await repository.ListAllAsync();
+
+            SelectList selectListItems = new SelectList(list.Where(filterExpression), nameof(IEntityBase.Id), dataTextField, null, dataGroupField);
+
+            return selectListItems;
+        }
+
+        public static async Task<SelectList> BuildSelectListExcludeArchivedAsync<T>(this IBaseArchiveableRepository<T> repository, string dataTextField, string dataGroupField = null) where T : class, IArchiveableEntity
+        {
+            var list = await repository.ListAllNonArchivedAsync();
+
+            SelectList selectListItems = new SelectList(list, nameof(IEntityBase.Id), dataTextField, null, dataGroupField);
+
+            return selectListItems;
+        }
+
+        public static async Task<SelectList> BuildFilteredSelectListExcludeArchivedAsync<T>(this IBaseArchiveableRepository<T> repository, Func<T, bool> filterExpression, string dataTextField, string dataGroupField = null) where T : class, IArchiveableEntity
+        {
+            var list = await repository.ListAllNonArchivedAsync();
+
+            SelectList selectListItems = new SelectList(list.Where(filterExpression), nameof(IEntityBase.Id), dataTextField, null, dataGroupField);
+
+            return selectListItems;
+        }
+    }
+}
