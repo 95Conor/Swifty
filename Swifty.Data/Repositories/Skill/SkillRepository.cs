6ed882e504146761067aeb35ab5937a72df1fe14
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-19 21:04:55 +0000

    Load referenced entities when Skill entity is loaded at repository level

--- Swifty.Data/Repositories/Skill/SkillRepository.cs
+++ Swifty.Data/Repositories/Skill/SkillRepository.cs
@@ -4,11 +4,58 @@ using System.Text;
 using System.Threading.Tasks;
 using Swifty.Core.Entities;
 using Swifty.Data.Context;
+using Swifty.Data.Contracts.Repositories;
 
 namespace Swifty.Data.Repositories
 {
     public class SkillRepository : SwiftyArchiveableRepository<Skill>
     {
-        public SkillRepository(SwiftyContext context) : base(context) { }
+        private readonly IBaseArchiveableRepository<SkillArea> _skillAreaRepository;
+        private readonly IBaseArchiveableRepository<SkillLevel> _skillLevelRepository;
+
+        public SkillRepository(SwiftyContext context, 
+            IBaseArchiveableRepository<SkillArea> skillAreaRepository,
+            IBaseArchiveableRepository<SkillLevel> skillLevelRepository) : base(context) 
+        {
+            _skillAreaRepository = skillAreaRepository;
+            _skillLevelRepository = skillLevelRepository;
+        }
+
+        public override async Task<List<Skill>> ListAllAsync()
+        {
+            var allEntities = await base.ListAllAsync();
+
+            foreach (var entity in allEntities)
+            {
+                try
+                {
+                    entity.Area = await _skillAreaRepository.GetByIdAsync(entity.SkillAreaId);
+                    entity.Level = await _skillLevelRepository.GetByIdAsync(entity.SkillLevelId);
+                }
+                catch (Exception ex)
+                {
+                    throw (ex);
+                }
+            }
+
+            return allEntities;
+        }
+
+        public override async Task<Skill> GetByIdAsync(int id)
+        {
+            var entity = await base.GetByIdAsync(id);
+
+            try
+            {
+                entity.Area = await _skillAreaRepository.GetByIdAsync(entity.SkillAreaId);
+                entity.Level = await _skillLevelRepository.GetByIdAsync(entity.SkillLevelId);
+            }
+            catch (Exception ex) 
+            {
+                throw (ex);
+            }
+
+            return entity;
+        }
     }
 }
