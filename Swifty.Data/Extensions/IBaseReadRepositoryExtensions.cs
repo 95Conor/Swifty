6c861aca5d3951714d1e2b4cfe0f5c1131cc353f
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-19 14:44:29 +0000

    Move IBaseReadRepositoryExtensions to Swifty.Data

--- Swifty.Data/Extensions/IBaseReadRepositoryExtensions.cs
+++ Swifty.Data/Extensions/IBaseReadRepositoryExtensions.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Swifty.Web.Extensions
+namespace Swifty.Data.Extensions
 {
     public static class IBaseReadRepositoryExtensions
     {
