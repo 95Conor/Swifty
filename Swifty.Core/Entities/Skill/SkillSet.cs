a6c53c508a5e16589fcedb6df4d128b94a7de325
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-20 00:17:07 +0000

    Potential hacky solution to easily see the Skill collection within a SkillSet entity rather than handling the SkillSetSkillLink collection

--- Swifty.Core/Entities/Skill/SkillSet.cs
+++ Swifty.Core/Entities/Skill/SkillSet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using Swifty.Core.Contracts.Entities;
 
@@ -9,6 +10,9 @@ namespace Swifty.Core.Entities
     {
         public int Id { get; set; }
 
-        public ICollection<SkillSetSkillLink> Set { get; set; }
+        public ICollection<SkillSetSkillLink> SkillLinkCollection { get; set; }
+
+        [NotMapped]
+        public List<Skill> Set { get; set; }
     }
 }
--- Swifty.Data/Repositories/Skill/SkillSetRepository.cs
+++ Swifty.Data/Repositories/Skill/SkillSetRepository.cs
@@ -1,13 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Swifty.Core.Entities;
 using Swifty.Data.Context;
+using Swifty.Data.Contracts.Repositories;
 
 namespace Swifty.Data.Repositories
 {
     public class SkillSetRepository : SwiftyRepository<SkillSet>
     {
-        public SkillSetRepository(SwiftyContext context) : base(context) { }
+        private IBaseArchiveableRepository<Skill> _skillRepository;
+
+        public SkillSetRepository(SwiftyContext context, IBaseArchiveableRepository<Skill> skillRepository) : base(context) 
+        {
+            _skillRepository = skillRepository;
+        }
+
+        public override async Task<List<SkillSet>> ListAllAsync()
+        {
+            var entities = await base.ListAllAsync();
+
+            foreach (var entity in entities)
+            {
+                await PersistSkillCollection(entity);
+            }
+
+            return entities;
+        }
+
+        public override async Task<SkillSet> GetByIdAsync(int id)
+        {
+            var entity = await base.GetByIdAsync(id);
+            await PersistSkillCollection(entity);
+            return entity;
+        }
+
+        // Hacky fix - ensure the collections are mapped and peristed when save/loading
+        private async Task PersistSkillCollection(SkillSet entity)
+        {
+            if (entity.SkillLinkCollection != null && entity.SkillLinkCollection.Count > 0)
+            {
+                List<Skill> skillEntitiesMapped = new List<Skill>();
+
+                foreach (var skillLink in entity.SkillLinkCollection)
+                {
+                    var skillEntity = await _skillRepository.GetByIdAsync(skillLink.SkillId);
+                    
+                    if (skillEntity != null)
+                    {
+                        skillEntitiesMapped.Add(skillEntity);
+                    }
+                }
+
+                entity.Set = skillEntitiesMapped;
+            }
+        }
     }
 }
