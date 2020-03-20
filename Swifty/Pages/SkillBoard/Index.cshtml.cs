1f217fa20a9163f7518367e4964bd0311f50cdf4
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-20 01:10:00 +0000

    Tidying up navbar links and added SkillBoard and SkillSnapshots folders/index skeleton

--- Swifty/Pages/SkillBoard/Index.cshtml.cs
+++ Swifty/Pages/SkillBoard/Index.cshtml.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Swifty.Web.Pages
+namespace Swifty.Web.Pages.SkillBoard
 {
-    [Authorize("IsAdmin")]
-    public class AdminPortalModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
