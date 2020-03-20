0899b00e7b1e3855e84400616f6578d421ff7608
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-20 00:57:47 +0000

    Improved navbar and added AdminPortal for admin related links

---
+++ Swifty/Pages/AdminPortal.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Swifty.Web
+{
+    [Authorize("IsAdmin")]
+    public class AdminPortalModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
