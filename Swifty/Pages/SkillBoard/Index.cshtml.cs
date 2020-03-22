6941c88a7205d56758587a307edfe69df5f7d512
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-22 15:37:10 +0000

    Added SkillService to SkillBoard index code-behind in preparation

--- Swifty/Pages/SkillBoard/Index.cshtml.cs
+++ Swifty/Pages/SkillBoard/Index.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Swifty.Core.Entities;
+using Swifty.Data.Contracts.Services;
 
 namespace Swifty.Web.Pages.SkillBoard
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ISkillService<Skill> _skillService;
 
+        public IndexModel(ISkillService<Skill> skillService)
+        {
+            _skillService = skillService;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var allSkillBySkillArea = await _skillService.ListAllNonArchivedSkillsBySkillArea();
+
+            return Page();
         }
     }
 }
