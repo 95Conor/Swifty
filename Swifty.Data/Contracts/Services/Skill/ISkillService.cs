8f7e210a51fbb246ee912431631ca4854e666e61
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-22 13:19:33 +0000

    SkillService implementation - Listing skills by skill area in preparation for SkillBoard UI

--- Swifty.Data/Contracts/Services/Skill/ISkillService.cs
+++ Swifty.Data/Contracts/Services/Skill/ISkillService.cs
@@ -1,11 +1,16 @@
 ﻿using Swifty.Core.Contracts.Entities;
+using Swifty.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Swifty.Data.Contracts.Services
 {
     public interface ISkillService<TEntity> : IEntityService<TEntity> where TEntity : class, IEntityBase
     {
+        public Task<Dictionary<SkillArea, List<Skill>>> ListAllSkillsBySkillArea();
+
+        public Task<Dictionary<SkillArea, List<Skill>>> ListAllNonArchivedSkillsBySkillArea();
     }
 }
--- Swifty.Data/Services/EntityServices/Skill/SkillService.cs
+++ Swifty.Data/Services/EntityServices/Skill/SkillService.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Text;
 using Swifty.Data.Contracts.Services;
 using Swifty.Core.Entities;
+using Swifty.Data.Contracts.Repositories;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Swifty.Data.Services
 {
     public class SkillService : ISkillService<Skill>
     {
+        private readonly IBaseArchiveableRepository<Skill> _skillRepository;
+
+        public SkillService(IBaseArchiveableRepository<Skill> skillRepository)
+        {
+            _skillRepository = skillRepository;
+        }
+
+        public async Task<Dictionary<SkillArea, List<Skill>>> ListAllSkillsBySkillArea()
+        {
+            List<Skill> allSkills = await _skillRepository.ListAllAsync();
+
+            Dictionary<SkillArea, List<Skill>> toReturn = new Dictionary<SkillArea, List<Skill>>();
+
+            foreach (Skill skill in allSkills)
+            {
+                if (toReturn.ContainsKey(skill.Area))
+                {
+                    toReturn[skill.Area].Add(skill);
+                }
+                else
+                {
+                    toReturn.Add(skill.Area, new List<Skill>() { skill });
+                }
+            }
+
+            return toReturn;
+        }
+
+        public async Task<Dictionary<SkillArea, List<Skill>>> ListAllNonArchivedSkillsBySkillArea()
+        {
+            List<Skill> allSkills = await _skillRepository.ListAllNonArchivedAsync();
+
+            Dictionary<SkillArea, List<Skill>> toReturn = new Dictionary<SkillArea, List<Skill>>();
+
+            foreach (Skill skill in allSkills)
+            {
+                if (toReturn.ContainsKey(skill.Area))
+                {
+                    toReturn[skill.Area].Add(skill);
+                }
+                else
+                {
+                    if (!skill.Area.IsArchived)
+                    {
+                        toReturn.Add(skill.Area, new List<Skill>());
+                    }
+                }
+            }
+
+            return toReturn;
+        }
     }
 }
