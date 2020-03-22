1ad31521304bb076d01fc0b0b0de3ebb15067f77
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-22 14:35:02 +0000

    Added Mapping base project

---
+++ Swifty.Mapping/Contracts/Base/IBaseMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swifty.Mapping.Contracts.Base
+{
+    public interface IBaseMapper
+    {
+        public void MapFrom<TSource, TDestination>() 
+            where TSource : class 
+            where TDestination : class;
+
+        public void MapTo<TSource, TDestination>()
+            where TSource : class
+            where TDestination : class;
+    }
+}
