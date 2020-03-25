9b2332c5cff01510dd0cb9e42bc0eb8c94e52450
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-25 23:01:25 +0000

    Added BaseViewModel for ViewModels. Also IndexViewModel skeleton for SkillBoard index page

---
+++ Swifty.Web/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swifty.Web.ViewModels.Base
+{
+    public class BaseViewModel
+    {
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+
+        public List<string> NeutralMessages { get; set; } = new List<string>();
+
+        public List<string> SuccessMessages { get; set; } = new List<string>();
+    }
+}
---
+++ Swifty.Web/ViewModels/SkillBoard/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swifty.Web.ViewModels.Base;
+
+namespace Swifty.Web.ViewModels.SkillBoard
+{
+    public class IndexViewModel : BaseViewModel
+    {
+
+    }
+}
