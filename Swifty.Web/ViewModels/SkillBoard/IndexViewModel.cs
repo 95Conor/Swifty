bfcb22697c733f8b7d4cb465a3a716751f426008
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-25 23:22:33 +0000

    Populated SkillBoard IndexViewModel with base property needed to display board

--- Swifty.Web/ViewModels/SkillBoard/IndexViewModel.cs
+++ Swifty.Web/ViewModels/SkillBoard/IndexViewModel.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Swifty.Web.ViewModels.Base;
+using Swifty.Web.ViewModels.Shared;
 
 namespace Swifty.Web.ViewModels.SkillBoard
 {
     public class IndexViewModel : BaseViewModel
     {
-
+        public Dictionary<SkillAreaViewModel, List<SkillViewModel>> SkillsByArea { get; set; }
     }
 }
