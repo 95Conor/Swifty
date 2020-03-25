6a8443fcd2d650222e114d56ffff27ffac2e447c
Conor Maher <Conor.Maher@tigerbay.co.uk>
2020-03-25 23:16:27 +0000

    Added shared viewmodels to hold Skill data for board displays

---
+++ Swifty.Web/ViewModels/Shared/SkillViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swifty.Web.ViewModels.Shared
+{
+    public class SkillViewModel
+    {
+        public int Id { get; set; }
+
+        public SkillAreaViewModel SkillArea { get; set; }
+
+        public SkillLevelViewModel SkillLevel { get; set; }
+
+        public string Detail { get; set; }
+    }
+}
